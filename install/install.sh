c1f8842e5b1b1293c973cfffb4791c171f68fb09
Herbert256 <herbert@groot.jebbink.nl>
2020-09-20 14:34:51 +0200

    no message

--- install/install.sh
+++ install/install.sh
@@ -2,7 +2,9 @@
 
 sudo service mariadb start
 sleep 2
-
 mysql < install/wsl2-phpxx.sql
-
 sudo service mariadb stop
+
+rm -Rf /var/www/html
+ln -s /app/www /var/www/html
+chown data:data /var/www/html
