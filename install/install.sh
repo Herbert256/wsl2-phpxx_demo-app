2bfeebe141dc98b77973879fb912b60b1c595950
Herbert256 <herbert@groot.jebbink.nl>
2020-09-22 23:22:25 +0200

    no message

--- install/install.sh
+++ install/install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-rm -Rf /var/www/html
 ln -s /app/www /var/www/html
-chown data:data /var/www/html
 
 mysql < install/install.sql
--- php/wsl2-phpxx.php
+++ php/wsl2-phpxx.php
@@ -1,12 +1,10 @@
 <?php
 
-
   $start = microtime (true);
 
   include 'memcached.php';
   include 'mariadb.php';
   include 'filesystem.php';
-
 
   function version ($check) {
 
@@ -16,6 +14,5 @@
       echo ' <b>***</b>';
 
   }
-
 
 ?>
